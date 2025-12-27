6318f444104a361bb01ebece6e1c0ea385050443
Kyle Mardell <151871777+KyleMardell@users.noreply.github.com>
2025-12-27 22:51:16 +0000

    Add a check for a user in the movie list context to fix error from the contect provider when not logged in.

--- src/app/context/UserMoviesContext.tsx
+++ src/app/context/UserMoviesContext.tsx
@@ -2,6 +2,7 @@
 
 import { createContext, useEffect, useState } from "react";
 import { getMovies } from "../lib/api";
+import { useAuth } from "../useAuth";
 
 export const UserMoviesContext = createContext<UserMoviesContextType | null>(null);
 
@@ -12,7 +13,9 @@ type UserMoviesContextType = {
 
 export const UserMoviesProvider = ({ children }: { children: React.ReactNode }) => {
     const [userMovies, setUserMovies] = useState<any[]>([]);
+    const { user} = useAuth();
 
+    
     useEffect(() => {
         const handleMount = async () => {
             try {
@@ -22,8 +25,12 @@ export const UserMoviesProvider = ({ children }: { children: React.ReactNode }) 
                 console.log(err);
             }
         };
+
+        if (user) {
         handleMount();
-    }, [])
+    }
+        
+    }, [user])
 
 
     return (
