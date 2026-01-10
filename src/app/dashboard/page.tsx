89c805283ff2bb4009f8a0bf819cb29905333a2b
Kyle Mardell <151871777+KyleMardell@users.noreply.github.com>
2026-01-10 16:53:12 +0000

    Update again.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,8 @@
 export const dynamic = "force-dynamic"; // critical
 export const fetchCache = "force-no-store"; // critical
 
-import DashboardClient from "./DashboardClient";
+import DashboardPage from "./DashboardClient";
 
 export default function Page() {
-    return <DashboardClient />;
+    return <DashboardPage />;
 }
